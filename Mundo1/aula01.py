4834ba9768551b397349807aee48404ceb7cd6fa
Carlos Eduardo <carloseducontatodev@gmail.com>
2025-03-18 15:29:23 -0300

    upd

---
+++ Mundo1/aula01.py
@@ -0,0 +1,16 @@
+# Desafio 1
+print("============ DESAFIO 1 ============")
+nome = input('Como é o seu nome? ')
+print("Bem vindo!", nome)
+# Desafio 2
+print("============ DESAFIO 2 ============")
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+print('Você nasceu dia', dia, 'de', mes, 'de', ano, '. Correto?')
+# Desafio 3
+print("============ DESAFIO 3 ============")
+num1 = int(input('1° Número: '))
+num2 = int(input('2° Número: '))
+soma = num1 + num2 
+print('Somando...', num1, '+', num2, "=", soma)
